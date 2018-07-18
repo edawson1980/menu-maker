8fc9525b79431a751c4d53a21d42c4baa3a5b02b
Erin <erinedawson@gmail.com>
2018-07-18 11:58:19 -0600

    add getters and setters for courses property

---
+++ app.js
@@ -0,0 +1,30 @@
+/*CREATE NEW MENU OBJECT*/
+
+let menu = {
+  /*menu has COURSES property, with three values*/
+  _courses: {
+    appetizers: [],
+    mains: [],
+    desserts: []
+  }
+  /*CREATE GETTERS AND SETTERS FOR EACH VALUE*/
+  get appetizers(){
+
+  },
+  set appetizers(appsInput){
+
+  },
+  get mains(){
+
+  },
+  set mains(mainsInput){
+
+  },
+  get desserts(){
+
+  },
+  set desserts(dessertInput){
+    
+  }
+
+}
